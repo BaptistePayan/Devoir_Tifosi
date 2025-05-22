14227d86ab619356182e7495b81fb59077ec3f5c
BaptistePayan <teck-bat@hotmail.fr>
2025-05-22 16:18:43 +0200

    Création de 6 tables avec clé primaire

--- script_tifosi.sql
+++ script_tifosi.sql
@@ -1,5 +1,47 @@
+-- Création de la base Tifosi, l'utilisateur et les privilèges
+
 CREATE DATABASE IF NOT EXISTS tifosi CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE tifosi;
 CREATE USER 'tifosi'@'localhost' IDENTIFIED BY 'TifosiFoccacia'; 
 GRANT ALL PRIVILEGES ON tifosi.* TO 'tifosi'@'localhost';
 FLUSH PRIVILEGES;
+
+-- Tables Primaires
+
+CREATE TABLE ingredient (
+    id_ingredient INT AUTO_INCREMENT PRIMARY KEY,
+    nom VARCHAR(50) NOT NULL UNIQUE
+);
+
+CREATE TABLE client (
+    id_client INT AUTO_INCREMENT PRIMARY KEY,
+    nom VARCHAR(50) NOT NULL ,
+    email VARCHAR(150) NOT NULL UNIQUE,
+    code_postal INT NOT NULL 
+)
+
+CREATE TABLE marque (
+    id_marque INT AUTO_INCREMENT PRIMARY KEY,
+    nom VARCHAR(50) NOT NULL UNIQUE
+);
+
+CREATE TABLE focaccia (
+    id_focaccia INT AUTO_INCREMENT PRIMARY KEY,
+    nom VARCHAR(50) NOT NULL UNIQUE,
+    prix DECIMAL(5,2) NOT NULL
+);
+
+CREATE TABLE menu (
+    id_menu INT AUTO_INCREMENT PRIMARY KEY,
+    nom VARCHAR(50) NOT NULL UNIQUE,
+    prix DECIMAL(5,2) NOT NULL
+);
+
+CREATE TABLE boisson (
+    id_table INT AUTO_INCREMENT PRIMARY KEY,
+    nom VARCHAR(50) NOT NULL UNIQUE
+)
+
+
+
+
